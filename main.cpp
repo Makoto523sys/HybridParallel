60ac9ce6e37da0fb89d12360bb020e69633d5c1a
Makoto <ookunimasaharu@gmail.com>
2021-08-31 09:48:00 +0900

    Non Parallel version

--- main.cpp
+++ main.cpp
@@ -1,33 +1,21 @@
 #include<iostream>
 #include<cmath>
-#include<omp.h>
 
 double f(double);
 const long int N = 1e9;
 
 int main(int argc, char **argv){
-    int rank, size, host_rank=0;
-    double sum, ans;
+    double ans;
     double a, b;
     a = 0.0; b = 1.0;
     double dx = (b-a) / N;
-    sum = 0.0;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int begin = N / size * rank;
-    int end = N / size * (rank + 1);
-    //std::cout << begin << " " << end << std::endl;
+    ans = 0.0;
 
 
-#pragma omp parallel for reduction(+:sum)
-    for(int i = begin; i < end; i++){
-        sum += dx * f(dx * i);
+    for(int i = 0; i < N; i++){
+        ans += dx * f(dx * i);
     }
-    //std::cout << std::abs(M_PI - sum) << std::endl;
-    MPI_Reduce(&sum, &ans, 1, MPI_DOUBLE, MPI_SUM, host_rank, MPI_COMM_WORLD);
-    if(rank == host_rank) std::cout << std::abs(M_PI / 8 - ans) << std::endl;
-    MPI_Finalize();
+    std::cout << std::abs(M_PI / 8 - ans) << std::endl;
     return 0;
 }
 
